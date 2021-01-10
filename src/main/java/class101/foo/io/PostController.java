753c320a19d481183502d0abe6a45e57c46af9bd
foo <foo.jun@kakao.com>
2021-01-10 18:47:23 +0900

    Added rabbitmq logic.

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -2,9 +2,7 @@ package class101.foo.io;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.bytebuddy.TypeCache;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -15,6 +13,8 @@ import java.util.List;
 @RestController
 public class PostController {
 
+    private static Integer PAGE_SIZE = 20;
+
     @Autowired
     PostRepository postRepository;
 
@@ -24,7 +24,8 @@ public class PostController {
     @Autowired
     ObjectMapper objectMapper;
 
-    private static Integer PAGE_SIZE = 20;
+    @Autowired
+    PostCacheService postCacheService;
 
     // 1. 글을 작성한다.
     @PostMapping("/post")
@@ -37,22 +38,24 @@ public class PostController {
     // 2. 글 목록을 페이징하여 반환
     @GetMapping("/posts")
     public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        return postRepository.findAll(
-                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-        );
+        if(page.equals(1)) {
+            return postCacheService.getFirstPostPage();
+        } else {
+            return postRepository.findAll(
+                    PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+            );
+        }
     }
-    
+
     // 3. 글 번호로 조회
     @GetMapping("/post/{id}")
     public Post getPostById(@PathVariable("id") Long id) {
         return postRepository.findById(id).get();
     }
-
+    
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
-    // git-test3
-
     @GetMapping("/search")
-    public List<Post> findPostByContent(@RequestParam String content) {
+    public List<Post> findPostsByContent(@RequestParam String content) {
         return postRepository.findByContentContains(content);
     }
 
