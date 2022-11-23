8f6a6cae04638e04d1f24ba7c92a98ca9bded521
91274750 <ppabew@icloud.com>
2022-11-23 14:13:02 +0900

    feat: Paging 적용

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -1,7 +1,11 @@
 package class101.foo.io;
 
+import net.bytebuddy.TypeCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,19 +16,22 @@ public class PostController {
     @Autowired
     PostRepository postRepository;
 
+
+    private static Integer PAGE_SIZE = 20;
+
     // 1. 글을 작성한다.
     @PostMapping("/post")
     public Post createPost(@RequestBody Post post) {
         return postRepository.save(post);
     }
 
-    // 2-1. 글 목록을 조회한다.
+    // 2. 글 목록을 페이징하여 반환
     @GetMapping("/posts")
-    public List<Post> getPostList() {
-        return postRepository.findAll();
+    public Page<Post> getPostList(@RequestParam Integer page) {
+        return postRepository.findAll(
+                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+        );
     }
-    
-    // 2-2 글 목록을 페이징하여 반환
     
     // 3. 글 번호로 조회
     
